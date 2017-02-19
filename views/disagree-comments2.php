7976c6951e876614448e370ff8ed67fbc017b381
MigsDomingo <miguelcdomingo@gmail.com>
2017-02-19 10:20:31 +0800

    Write-ups
    Sample webpage

--- views/disagree-comments2.php
+++ views/disagree-comments2.php
@@ -4,8 +4,8 @@
 	</div>
 	<div class="media-body">
 		<h4 class="media-heading"><a href="#">Gabriel Ong</a> <small><i>Posted on February 21, 2016</i></small></h4>
-		<p><span class="source"><b>Source: </b><a href="http://memes.com">http://memes.com</a><br></span>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+		<p><span class="source"><b>Source: </b><a href="http://education.seattlepi.com/advantages-traditional-schools-2140.html">http://education.seattlepi.com/advantages-traditional-schools-2140.html</a><br></span>
+			Disagree: my neighbors' children are homeschooled, and they lack some of life's important skills! See source provided.</p>
 		<!-- Nested media object -->
 		<div class="media">
 			<div class="media-left">
@@ -13,8 +13,8 @@
 			</div>
 			<div class="media-body">
 				<h4 class="media-heading"><a href="#">Jack Tan</a> <small><i>Posted on February 21, 2016</i></small></h4>
-				<p><span class="source"><b>Source: </b><a href="http://memes.com">http://memes.com</a><br></span>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				<p><span class="source"><b>Source: </b><a href="http://sixmoresummers.com/10-reasons-why-public-schools-are-awesome/">http://sixmoresummers.com/10-reasons-why-public-schools-are-awesome/</a><br></span>
+				I've been wondering myself, and indeed, your source led me to find this. it provided the argument that regular-schooling is better than home-schooling. Thumbs up!</p>
 
 				<!-- Nested media object -->
 				<div class="media">
@@ -23,8 +23,8 @@
 					</div>
 					<div class="media-body">
 						<h4 class="media-heading"><a href="#">Mercy Gem</a> <small><i>Posted on February 21, 2016</i></small></h4>
-						<p><span class="source"><b>Source: </b><a href="http://memes.com">http://memes.com</a><br></span>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+						<p><span class="source"><b>Source: </b><a href="https://sites.google.com/site/daniellemahekseportfolio/why-public-schooling-is-the-best-option">v</a><br></span>
+						Thank you for this: all my relatives has been convincing me to home-school my kids, yet studies like these debunk their claims.</p>
 					</div>
 				</div>
 			</div>
@@ -35,8 +35,8 @@
 				</div>
 				<div class="media-body">
 					<h4 class="media-heading"><a href="#">Zarya Vladimir</a> <small><i>Posted on February 21, 2016</i></small></h4>
-					<p><span class="source"><b>Source: </b><a href="http://memes.com">http://memes.com</a><br></span>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+					<p><span class="source"><b>Source: </b><a href="https://www.publicschoolreview.com/blog/10-advantages-to-public-education">https://www.publicschoolreview.com/blog/10-advantages-to-public-education</a><br></span>
+					Regular schooling teaches your kids some of life's skills you cannot teach at home.</p>
 				</div>
 			</div>
 
@@ -50,8 +50,8 @@
 		</div>
 		<div class="media-body">
 			<h4 class="media-heading"><a href="#">Popito Tapato</a> <small><i>Posted on February 21, 2016</i></small></h4>
-			<p><span class="source"><b>Source: </b><a href="http://memes.com">http://memes.com</a><br></span>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			<p><span class="source"><b>Source: </b><a href="https://www.screenflex.com/reasons-why-year-round-school-is-a-good-idea/">https://www.screenflex.com/reasons-why-year-round-school-is-a-good-idea/</a><br></span>
+			TL;DR of this source: regular schooling makes your kids more equipped to life, particularly with social events.</p>
 		</div>
 	</div>
 </div>
